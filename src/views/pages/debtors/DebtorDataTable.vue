c5ab20731e608e23d09eae5512ddd1b45858c7e8
John Richard <41709492+jmalabs@users.noreply.github.com>
2023-12-26 08:47:29 +0800

    Update DebtorDataTable.vue
    Add title to debtor table.

--- src/views/pages/debtors/DebtorDataTable.vue
+++ src/views/pages/debtors/DebtorDataTable.vue
@@ -1,7 +1,7 @@
 
 
 <template>
-    <VCard>
+    <VCard title="Debtors">
         <VTable :headers="headers" :items="applications" item-key="fullName" class="table-rounded" hide-default-footer
             disable-sort>
             <thead>
